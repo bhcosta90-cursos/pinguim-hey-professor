c5cb0f6292b2a93574b121f9694a432cbc3ccb6a
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-05 10:23:04 -0300

    HEY-9: separando em componentes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,30 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <x-header>{{ __('Dashboard') }}</x-header>
     </x-slot>
 
-    <div class="py-12">
-        <form method="POST" action="{{route('question.store')}}">
-            @csrf
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="mb-4">
-                    <textarea
-                        name="question"
-                        placeholder="{{ __("Ask me anything") }}..."
-                        id="message"
-                        rows="4"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here...">{{old('question')}}</textarea>
-                    @error('question')
-                          <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{$message}}</p>
-                    @enderror
-                </div>
-
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Cadastrar pergunta
-                </button>
-            </div>
-        </form>
-    </div>
+    <x-container>
+        <x-forms.form post :action="route('question.store')">
+            <x-forms.textarea name="question" :place="__('Ask me anything')" />
+            <x-forms.button.primary>Cadastrar pergunta</x-forms.button.primary>
+            <x-forms.button.reset>Cancelar</x-forms.button.reset>
+        </x-forms.form>
+    </x-container>
 </x-app-layout>
