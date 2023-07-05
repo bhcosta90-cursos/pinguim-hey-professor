4913ef09ceae602e13f93c17eb49d5a396f9c930
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-05 09:44:51 -0300

    HEY-9: testing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
                           <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{$message}}</p>
                     @enderror
                 </div>
-a
+
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                     Cadastrar pergunta
                 </button>
