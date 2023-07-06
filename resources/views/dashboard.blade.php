0500871347c38efe5deba5095e3e2c517695ad91
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-06 12:06:47 -0300

    HEY-10: add frontend

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,11 +6,16 @@
     <x-container>
         <div class="dark:text-gray-300 uppercase font-bold">List of questions</div>
         <div class="dark:text-gray-400 space-y-4">
+            <form action="{{ route('dashboard') }}" method="get" class="flex items-center space-x-2">
+                <x-text-input type="text" name="search" value="{{ request()->search }}" class="w-full"/>
+                <x-btn.primary type="submit">Search</x-btn.primary>
+            </form>
+
             @foreach($questions as $item)
                 <x-question :question="$item" />
             @endforeach
 
-            {!! $questions->links() !!}
+            {!! $questions->appends(request()->except('_token'))->links() !!}
         </div>
 
     </x-container>
